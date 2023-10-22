bb2e68b766e645e124a3f945590715d40ccc2ff9
Carlos Zahid <zahidhdez@outlook.com>
2023-10-22 12:59:15 -0600

    ruta para agregar empleado

--- routes/employee.js
+++ routes/employee.js
@@ -1,4 +1,23 @@
-//Agregar empleado
 //Modificar empleado
 //Eliminar empleado
 //Buscar empleado por nombre
+
+const express = require('express');
+const mply = express.Router();
+const db = require('../config/database');
+
+//Agregar empleado
+mply.post("/alta", async (req, res, next) => {
+    const {mply_name,mply_lastname, mply_phone, mply_mail,mply_address} = req.body;
+    if(mply_name,mply_lastname && mply_phone && mply_mail && mply_address){
+        let query = `INSERT INTO hr(employee_name, employee_surnames, employee_phone, employee_email, employee_address) `;
+        query += `VALUES('${mply_name}','${mply_lastname}','${mply_phone}','${mply_mail}','${mply_address}')`
+
+        const rows = await db.query(query);
+        if(rows.affectedRows == 1){
+            return res.status(201).json({code: 201, message: 'registro del empleado exitoso'})
+        }
+        return res.status(500).json({code: 500, message: 'hubo un error al realizar la operacion'})
+    }
+    return res.status(500).json({code: 500, message: 'campos incompletos'})
+})
